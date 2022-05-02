7e796edbfcb395c211401983c3713161346bb8fb
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-05-01 21:42:52 -0400

    commit tree 7e84e72ca67359411fc85cc2a6914fbe3dabd3c6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event)  {
+    let cardArray = [];
+    for (let i = 0; i < names.length; i++) {
+          cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      if (i > 1)  {
+      console.log(cardArray);
+        return cardArray;
+    }
+    }
+    
+    
+  }
+  writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  
+  
+  
+  function countDown(number)  {
+      while (number > -1)  {
+      console.log(number--)
+      };
+  }
+  countDown(4);
